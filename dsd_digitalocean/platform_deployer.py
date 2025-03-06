ce9df0a3d5fc280a0ec022ff6b14f59ad99b3005
Eric Matthes <ehmatthes@gmail.com>
2025-03-06 11:24:15 -0500

    Supports automate-all on macOS, at proof of concept stage. (#9)
    * Added requirements.txt, added paramiko to requirements.
    
    * Draft paramiko code.
    
    * Better structure for running commands.
    
    * Reports progress.
    
    * Note may take few minutes to update server.
    
    * Started utils module.
    
    * Remove unneeded code.
    
    * Updates and restarts computer if required.
    
    * Don't update server when testing.
    
    * Util function run_server_cmd_ssh() writes output.
    
    * Can suppress output for server commands.
    
    * Util function reboot_if_required().
    
    * Util function reboot_server(), remove unused code.
    
    * Initial work to add a non-root user.
    
    * Adds non-root user if needed, does all work through that user.
    
    * Modify sudoers.d after adding new user.
    
    * Use modern reboot command.
    
    * Allow django_user to set env var.
    
    * Installs uv.
    
    * Properly reports username that was added.
    
    * Installs Python from full path to uv.
    
    * Initializes git repository, writes post-receive hook.
    
    * Makes hook executable.
    
    * Add remote to local project, run push command locally.
    
    * Troubleshooting.
    
    * git push remote main command works, except requires manual password entry.
    
    * Separate util function for pushing project code to server.
    
    * Configures firewall.
    
    * Allow connections on port 8000.
    
    * First-pass changes to project code.
    
    * Writes serve_project.sh file, and runs that file on server.
    
    * Serves project, using only gunicorn.
    
    * Remove unused code.
    
    * Fails to copy gunicorn service files to server.
    
    * Places gunicorn.socket in appropriate location.
    
    * Correct gunicorn files; does not seem to start the service though.
    
    * Uses paramiko to copy file to server.
    
    * More specific systemctl commands in sudoers.d.
    
    * Configure git to use main before running init.
    
    * Attempt to install and configure Caddy.
    
    * Add Caddyfile.
    
    * Call add caddyfile fn, update sudo perms for working with caddy.
    
    * Use caddy to proxy gunicorn, debug true, allow 80 and 443 tcp connections.
    
    * Don't disabel update.
    
    * Start gunicorn service before caddy.
    
    * Caddy needs to proxy gunicorn socket, not port 8000.
    
    * Set env vars in gunicorn.service.
    
    * Opens project in new chrome tab, on macos.
    
    * More resilient handling of systemctl services.
    
    * Show output of quick commands.
    
    * Set env vars in serve_project.sh, allow restart gunicorn and caddy.
    
    * Use ssh keypair for git push.
    
    * Import command error.

--- dsd_digitalocean/platform_deployer.py
+++ dsd_digitalocean/platform_deployer.py
@@ -1,7 +1,16 @@
 """Manages all Digital Ocean-specific aspects of the deployment process.
 
-Notes:
+VPS notes:
+
+- All actions taken against the server should be idempotent if at all possible. If an
+  action is not idempotent, that should be noted.
+
+DO notes:
+
 - 
+
+
+General plugin development notes:
 
 Add a new file to the user's project, without using a template:
 
@@ -44,13 +53,16 @@ Add a set of requirements:
 """
 
 import sys, os, re, json
+import time
 from pathlib import Path
+import tempfile
 
 from django.utils.safestring import mark_safe
 
 import requests
 
 from . import deploy_messages as platform_msgs
+from . import utils as do_utils
 
 from django_simple_deploy.management.commands.utils import plugin_utils
 from django_simple_deploy.management.commands.utils.plugin_utils import dsd_config
@@ -76,12 +88,21 @@ class PlatformDeployer:
         self._validate_platform()
         self._prep_automate_all()
 
-        # Configure project for deployment to Digital Ocean.
 
-        # Update server.
-        # Run a read-only command through SSH.
-        print("HERE")
-        breakpoint()
+        # Configure server.
+        self._connect_server()
+        self._update_server()
+        self._setup_server()
+        
+        # Configure project for deployment to Digital Ocean.
+        self._add_requirements()
+        self._modify_settings()
+        self._add_serve_project_file()
+
+        self._add_caddyfile()
+        self._configure_gunicorn()
+
+
 
         self._conclude_automate_all()
         self._show_success_message()
@@ -104,6 +125,160 @@ class PlatformDeployer:
         pass
 
 
+    def _connect_server(self):
+        """Make sure we can connect to the server, with an appropriate username."""
+        do_utils.set_server_username()
+        do_utils.configure_firewall()
+
+
+    def _update_server(self):
+        """Update the server."""
+        # Don't update during unit and integration testing.
+        plugin_utils.write_output("Updating server (this may take a few minutes)...")
+        if dsd_config.unit_testing:
+            plugin_utils.write_output("  (skipped during testing)")
+            return
+
+        # Run update command.
+        cmd = "sudo apt-get update && sudo DEBIAN_FRONTEND=noninteractive apt-get full-upgrade -y"
+        stdout, stderr = do_utils.run_server_cmd_ssh(cmd)
+        plugin_utils.write_output("  Finished updating server.")
+
+        # See if we need to reboot. If rebooted, check for updates again. This is most
+        # likely needed with a fresh VM, after its first round of updates.
+        rebooted = do_utils.reboot_if_required()
+        if rebooted:
+            self._update_server()
+
+    def _setup_server(self):
+        """Run initial server setup.
+
+        Roughly follows a standard Ubuntu server setup guide, such as:
+        - https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu
+        """
+        # DEV: Disable during development.
+        do_utils.install_uv()
+        do_utils.install_python()
+        do_utils.configure_git(self.templates_path)
+        do_utils.install_caddy()
+
+    def _add_requirements(self):
+        """Add server-specific requirements."""
+        plugin_utils.write_output("  Adding server-specific requirements...")
+        requirements = ["gunicorn"]
+        plugin_utils.add_packages(requirements)
+
+    def _modify_settings(self):
+        # Add do-specific settings.
+        template_path = self.templates_path / "settings.py"
+        context = {
+            "deployed_project_name": dsd_config.local_project_name,
+            "ip_addr": os.environ.get("DSD_HOST_IPADDR"),
+        }
+        plugin_utils.modify_settings_file(template_path, context)
+
+    def _add_serve_project_file(self):
+        # Add a bash script to start server process after code pushes.
+        # template_path = self.templates_path / "dockerfile_example"
+        # context = {
+        #     "django_project_name": dsd_config.local_project_name,
+        # }
+        # contents = plugin_utils.get_template_string(template_path, context)
+
+        # # Write file to project.
+        # path = dsd_config.project_root / "Dockerfile"
+        # plugin_utils.add_file(path, contents)
+
+
+        template_path = self.templates_path / "serve_project.sh"
+        project_path = Path(f"/home/{dsd_config.server_username}/{dsd_config.local_project_name}")
+        context = {
+            "project_path": project_path,
+            "uv_path": f"/home/{dsd_config.server_username}/.local/bin/uv",
+        }
+        contents = plugin_utils.get_template_string(template_path, context)
+
+        # Write file to project.
+        path = dsd_config.project_root / "serve_project.sh"
+        plugin_utils.add_file(path, contents)
+
+    def _add_caddyfile(self):
+        """Add a Caddyfile to the project.
+
+        This configures Caddy, for serving static files.
+        """
+        template_path = self.templates_path / "Caddyfile"
+        context = {
+            "server_ip_address": os.environ.get("DSD_HOST_IPADDR")
+        }
+        contents = plugin_utils.get_template_string(template_path, context)
+
+        with tempfile.NamedTemporaryFile() as tmp:
+            path_local = Path(tmp.name)
+            path_local.write_text(contents)
+
+            path_remote = f"/home/{dsd_config.server_username}/Caddyfile"
+            do_utils.copy_to_server(path_local, path_remote)
+
+            cmd = f"sudo mv /home/{dsd_config.server_username}/Caddyfile /etc/caddy/Caddyfile"
+            do_utils.run_server_cmd_ssh(cmd)
+
+
+
+
+
+    def _configure_gunicorn(self):
+        """Configure gunicorn to run as a system service.
+
+        DEV: This should probably go somewhere else.
+        """
+        plugin_utils.write_output("  Configuring gunicorn to run as a service.")
+
+        # Write gunicorn.socket to accessible location, then move it to appropriate location.
+        template_path = self.templates_path / "gunicorn.socket"
+        # cmd = f"scp {template_path} {dsd_config.server_username}@{os.environ.get("DSD_HOST_IPADDR")}:/home/{dsd_config.server_username}/gunicorn.socket"
+        # plugin_utils.write_output(cmd)
+        # plugin_utils.run_quick_command(cmd)
+
+        path_local = self.templates_path / "gunicorn.socket"
+        path_remote = f"/home/{dsd_config.server_username}/gunicorn.socket"
+        do_utils.copy_to_server(path_local, path_remote)
+
+        cmd = f"sudo mv /home/{dsd_config.server_username}/gunicorn.socket /etc/systemd/system/gunicorn.socket"
+        do_utils.run_server_cmd_ssh(cmd)
+
+        # gunicorn.service
+        template_path = self.templates_path / "gunicorn.service"
+        project_path = Path(f"/home/{dsd_config.server_username}/{dsd_config.local_project_name}")
+        context = {
+            "server_username": dsd_config.server_username,
+            "project_path": project_path,
+            "project_name": dsd_config.local_project_name,
+        }
+        contents = plugin_utils.get_template_string(template_path, context)
+        with tempfile.NamedTemporaryFile() as tmp:
+            path_local = Path(tmp.name)
+            path_local.write_text(contents)
+
+            # cmd = f"scp {path.as_posix()} {dsd_config.server_username}@{os.environ.get("DSD_HOST_IPADDR")}:/etc/systemd/system/gunicorn.service"
+            # plugin_utils.write_output(cmd)
+            # plugin_utils.run_quick_command(cmd)
+
+            # cmd = f"scp {path} {dsd_config.server_username}@{os.environ.get("DSD_HOST_IPADDR")}:/home/{dsd_config.server_username}/gunicorn.service"
+            # plugin_utils.write_output(cmd)
+            # plugin_utils.run_quick_command(cmd)
+
+            path_remote = f"/home/{dsd_config.server_username}/gunicorn.service"
+            do_utils.copy_to_server(path_local, path_remote)
+
+
+
+            cmd = f"sudo mv /home/{dsd_config.server_username}/gunicorn.service /etc/systemd/system/gunicorn.service"
+            do_utils.run_server_cmd_ssh(cmd)
+
+
+
+
     def _conclude_automate_all(self):
         """Finish automating the push to Digital Ocean.
 
@@ -119,8 +294,24 @@ class PlatformDeployer:
         # Push project.
         plugin_utils.write_output("  Deploying to Digital Ocean...")
 
+        do_utils.push_project()
+        # Make serve script executable.
+        project_path = Path(f"/home/{dsd_config.server_username}/{dsd_config.local_project_name}")
+        serve_script_path = f"{project_path}/serve_project.sh"
+        cmd = f"chmod +x {serve_script_path}"
+        do_utils.run_server_cmd_ssh(cmd)
+
+        do_utils.serve_project()
+
         # Should set self.deployed_url, which will be reported in the success message.
-        pass
+        self.deployed_url = f"http://{os.environ.get('DSD_HOST_IPADDR')}/"
+        cmd = f"open -a 'Google Chrome' {self.deployed_url}"
+        output_obj = plugin_utils.run_quick_command(cmd)
+        stdout, stderr = output_obj.stdout.decode(), output_obj.stderr.decode()
+        plugin_utils.write_output(stdout)
+        if stderr:
+            plugin_utils.write_output("--- Error ---")
+            plugin_utils.write_output(stderr)
 
     def _show_success_message(self):
         """After a successful run, show a message about what to do next.
